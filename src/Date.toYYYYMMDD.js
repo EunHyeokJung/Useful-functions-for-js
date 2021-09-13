b5819b7bbfa6612721508960735b97506ca8a854
EunHyeukJung <69712631+EunHyeokJung@users.noreply.github.com>
2021-09-13 09:33:21 +0900

    Update Date.toYYYYMMDD.js

--- src/Date.toYYYYMMDD.js
+++ src/Date.toYYYYMMDD.js
@@ -2,7 +2,7 @@ Date.prototype.toYYYYMMDD = function() {
 
     var MM = this.getMonth() + 1;
     var DD = this.getDate();
-    var result = this.getFullYear() + ("0"+MM).slice(-2) + ("0"+DD).slice(-2);
+    var result = this.getFullYear() + ("0" + MM).slice(-2) + ("0" + DD).slice(-2);
 
     return result;
 }
